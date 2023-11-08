8c42eee360a710249419a5443cc27270bbcf47b2
marcelo7marques <143418528+marcelo7marques@users.noreply.github.com>
2023-11-08 15:23:38 -0300

    Questao09.c

--- Questao09.c
+++ Questao09.c
@@ -13,7 +13,7 @@ scanf("%d",&num);
 switch (num) {
 
 case 1:
-printf("Numero não primo ! \n");
+printf("Número não primo ! \n");
 break;
 
 case 2:
@@ -29,30 +29,30 @@ printf("Número não Primo ! \n");
 break;
 
 case 5:
-printf("Numero Primo ! \n");
+printf("Número Primo ! \n");
 break;
 
 case 6:
-printf("Numero não Primo ! \n");
+printf("Número não Primo ! \n");
 break;
 
 case 7:
-printf("Numero Primo ! \n");
+printf("Número Primo ! \n");
 break;
 
 case 8:
-printf("Numero não Primo ! \n");
+printf("Número não Primo ! \n");
 break;
 
 case 9:
-printf("Numero não Primo ! \n");
+printf("Número não Primo ! \n");
 break;
 
 default: 
   if (num <=1 || num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0) {
-    printf("Numero não Primo ! \n");
+    printf("Número não Primo ! \n");
   } else {
-    printf ("Numero Primo ! \n");
+    printf ("Número Primo ! \n");
   }
 }
 return 0;
